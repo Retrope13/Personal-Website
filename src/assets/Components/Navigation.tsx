828b8cc120f8d7bf1c068c7d16755316be3260f7
Retrope13 <89430082+Retrope13@users.noreply.github.com>
2023-05-31 12:35:44 -0600

    remove useless imports

--- src/assets/Components/Navigation.tsx
+++ src/assets/Components/Navigation.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import {motion} from 'framer-motion';
 import { MenuItem } from './MenuItem';
-import { isNotEmittedStatement } from 'typescript';
 
 const variants = {
   open: {
