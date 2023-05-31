36a0a82d064653ceb3bf59f98e3b71c0b5018f21
Retrope13 <89430082+Retrope13@users.noreply.github.com>
2023-05-31 16:20:02 -0600

    set up more stuff for sideNav

--- src/assets/Components/MenuItem.tsx
+++ src/assets/Components/MenuItem.tsx
@@ -1,5 +1,7 @@
-import * as React from 'react';
 import {motion} from 'framer-motion';
+import { Link } from 'react-router-dom';
+import React, {useContext} from 'react';
+import Home from '../imgs/Home_Icon.png';
 
 interface MenuItemProps {
     i: number;
@@ -24,15 +26,18 @@ const variants = {
 
 };
 
+const pages = ["/", "/Projects", "/Contact"]
+const titles = ["Home", "Projects", "Contact Me"]
+
 export const MenuItem: React.FC<MenuItemProps> = ({ i }) => {
-    const style = { border: `2px solid #3AAFA9`};
+    const style = { border: `2px solid #3AAFA9`, decoration: "none"};
     return (
         <motion.li 
         variants = {variants}
         whileHover={{scale: 1.1}}
         whileTap = {{scale: 0.95}} >
-        <div className="icon-placeholder" style={style}/>
-        <div className = "text-placeholder" style={style}/>
+        <img src={Home} className="icon-placeholder" style={style}/>
+        <Link to={pages[i]} className='text-placeholder' style={style}> {titles[i]} </Link>
         </motion.li>
     );
 };
