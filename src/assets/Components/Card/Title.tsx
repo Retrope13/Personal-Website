900bc715c15abafb1f3ed6f285ccd0f2288ed714
Retrope13 <89430082+Retrope13@users.noreply.github.com>
2023-06-03 17:22:56 -0600

    Workaround for useInvertedScale

--- src/assets/Components/Card/Title.tsx
+++ src/assets/Components/Card/Title.tsx
@@ -9,22 +9,28 @@ interface TitleProps {
 }
 
 export const Title: React.FC<TitleProps> = ({ title, category, isSelected}) => {
-    const x = isSelected ? 30 : 15;
-    const y = x;
-    const scale = isSelected ? 1 : .8;
-
-    return (
-        <motion.div className = 'title-container'
-        initial={false}
-        animate={{x, y}}
-        transition = {isSelected ? openSpring : closeSpring}
-        transformTemplate={scaleTranslate}
-        style={{originX: 0, originY: 0}} >
-            <span className='category'>{category}</span>
-            <h2>{title}</h2>
+      const x = isSelected ? 30 : 15;
+      const y = x;
+    
+      return (
+        <motion.div
+          className="title-container"
+          initial={false}
+          animate={{ x, y }}
+          transition={isSelected ? openSpring : closeSpring}
+          transformTemplate={scaleTranslate}
+          style={{
+            transform: `scale(1, -1) translate(${x}px, ${y}px) translateZ(0)`,
+            transformOrigin: "top left",
+            originX: 0,
+            originY: 0,
+          }}
+        >
+          <span className="category">{category}</span>
+          <h2>{title}</h2>
         </motion.div>
-    );
-};
+      );
+    };
 
 const scaleTranslate =({x, y, scale}: {x: number; y: number; scale: number}) =>
 `scale(${scale}) translate(${x}px, ${y}px) translateZ(0)`;
