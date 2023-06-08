7b02691a14e9399db56e6500c9d1f02bf8019586
Samuel Mckay <89430082+Retrope13@users.noreply.github.com>
2023-06-08 15:13:14 -0600

    update to createRoot.render(). It's newer

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client'
 import './index.css';
 import Home from './Home';
 import reportWebVitals from './reportWebVitals';
@@ -7,18 +8,20 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Projects from './Projects';
 import Contact from './Contact';
 
-ReactDOM.render(
+
+const container = document.getElementById('root');
+const root = createRoot(container!);
+root.render(
   <React.StrictMode>
     <Router>
       <Routes>
       <Route path='/' element={<Home/>}/>
-      <Route path='/Projects' element={<Projects/>}/>
+      <Route path='/Projects/*' element={<Projects/>}/>
       <Route path='/Contact' element={<Contact/>}/>
       <Route path="*" element={<Home/>}/>
       </Routes>
     </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 reportWebVitals();
