5f01a1b0729711e788a645291409460511bafae7
Samuel Mckay <89430082+Retrope13@users.noreply.github.com>
2023-06-08 11:50:13 -0600

    change to Home

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './Home';
 import reportWebVitals from './reportWebVitals';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Projects from './Projects';
@@ -11,9 +11,10 @@ ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Routes>
-      <Route path='/' element={<App/>}/>
+      <Route path='/' element={<Home/>}/>
       <Route path='/Projects' element={<Projects/>}/>
       <Route path='/Contact' element={<Contact/>}/>
+      <Route path="*" element={<Home/>}/>
       </Routes>
     </Router>
   </React.StrictMode>,
