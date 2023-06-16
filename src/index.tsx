0168b1cf34ac8d1c0715ed62fc3c2df62a455f3f
Samuel Mckay <89430082+Retrope13@users.noreply.github.com>
2023-06-15 20:14:48 -0600

    Add firebase config to index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import {createRoot} from 'react-dom/client'
 import './index.css';
 import Home from './Home';
@@ -8,6 +7,23 @@ import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Projects from './Projects';
 import Contact from './Contact';
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDBlw4rhG_IeQya-voMmzVN7CQoY9X6EL0",
+  authDomain: "sam-mckay-personal-website.firebaseapp.com",
+  databaseURL: "https://sam-mckay-personal-website-default-rtdb.firebaseio.com",
+  projectId: "sam-mckay-personal-website",
+  storageBucket: "sam-mckay-personal-website.appspot.com",
+  messagingSenderId: "823000142706",
+  appId: "1:823000142706:web:37a19942634845725543eb",
+  measurementId: "G-Y0954CRPMK"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
