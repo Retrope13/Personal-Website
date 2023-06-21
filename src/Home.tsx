e137a45cb4e66f155f5faddc72ab6a3070058d8c
Samuel Mckay <89430082+Retrope13@users.noreply.github.com>
2023-06-21 13:41:48 -0600

    Get the lists to be a little more dynamic

--- src/Home.tsx
+++ src/Home.tsx
@@ -31,9 +31,10 @@ const TextDiv = styled.div`
   border: .15vw solid #FFFFFF;
   border-radius: 2vw;
   height: 400px;
+  display: inline-block;
   background-color: #1b2636;
   justify-content: center;
-  align-items: center;
+  align-items: flex-start;
   width: 60vw;
   padding-left: 1vw;
   margin-left: 27vw;
@@ -44,11 +45,11 @@ const Para = styled.p`
   font-family: Alexandria;
   font-size: 20px;
   color: #FFFFFF;
-  display: flex-box;
   align-items: flex-start;
-  width: auto;
+  width: 99%;
   height: 19vh;
-  line-height: 130%;
+  line-height: auto;
+  white-space: pre-line;
 `
 
 const sidebar ={
@@ -103,10 +104,10 @@ function Home() {
           <div className='image-container'>
            <img src={MyPhoto} alt="Me at Meow Wolf sitting in a semi-circular green chair with twisting tubes behind me!"/> {/*You can put a div behind the image to give it some depth*/}
           </div>
-      <TextDiv style={{ marginTop: '-59vh', paddingTop: '3vh', display: 'flex'}}>      
+      <TextDiv>      
           <TextHead className='bio-heading'>A bit about me</TextHead>
         <div className='bio-container'>
-        <Para style={{width: '50vw'}}>
+        <Para style={{width: '99%'}}>
         Welcome to my website! I am Sam McKay, a 22-year-old college graduate with a degree in Computer Science from CSU! 
         I love user experience/user interface design, and website development focused on accessibility so this website was a way to sharpen some of the tools in my set and learn some things too!
         I pursued a degree in computer science because of the potential it has as a tool to help people and the creative liberties that it affords developers.
@@ -119,8 +120,10 @@ function Home() {
        </div>
 
        <div className='CSU-container'>
-          <Para style={{width: '99%'}}>While enrolled at CSU, I earned a 3.71 cumulative GPA, my Bachelor's in Computer Science, and had the opportunity to work on several projects which you can see on the projects page of this website.
+          <Para >While enrolled at CSU, I earned a 3.71 cumulative GPA, my Bachelor's in Computer Science, and had the opportunity to work on several projects which you can see on the projects page of this website.
            Within many of these projects, I took on a leadership role and gained a firm grasp of various languages, libraries, and concepts, which have greatly assisted me in my journey as a developer.
+          {"\n"}
+
             <ul className='Languages'>
               <li className='ulTitleL'>Languages:</li>
               <li>&#x25CF; Javascript</li>
@@ -142,7 +145,7 @@ function Home() {
               <li>&#x25CF; Object-Oriented design</li>
               <li>&#x25CF; Game design</li>
               <li>&#x25CF; VR development</li>
-              <li>&#x25CF; Machine learning</li>
+              <li>&#x25CF; Machine Learning</li>
               <li>&#x25CF; Team management</li>
               <li>&#x25CF; Time management</li>
               <li>&#x25CF; Language acquisition</li>
