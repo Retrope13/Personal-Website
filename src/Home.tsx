57dd96e9096999986276e08c329019bba1f441b7
Samuel Mckay <89430082+Retrope13@users.noreply.github.com>
2023-12-05 20:05:33 -0700

    Add heading text to the webpage.

--- src/Home.tsx
+++ src/Home.tsx
@@ -5,14 +5,16 @@ import styled from 'styled-components';
 
 
 const Head = styled.h1`
-  color: #FFFFFF;
+  color: #ce7052; 
   margin-top: 0vh;
   min-width: 100px;
   font-family: Bavista;
   font-size: 100px;
   background-color: #00000000;
-  border-radius: 5px;
   height: auto;
+  text-shadow: 
+    2px 2px 0 #dc9954, /* Red shadow */
+    4px 4px 0 #d3c598; /* Green shadow */
   `
 
 function Home() {
@@ -21,19 +23,20 @@ function Home() {
   window.onscroll = function(){
     let progressHeight = (window.pageYOffset / totalHeight) * 100;
     if (progress) {
-      progress.style.height = progressHeight + "%" 
+      progress.style.height = progressHeight + "%"  
     }
   }
 
   return (
     <div className="App">
+      <div className="Heading"><Head>fdsafsdaf</Head></div>
       <title>Home</title>
       <body>
         <div id="progressbar1">
         </div>
-        <div id="scrollpath"></div>
+        <div id="scrollpath"></div> 
+          <h2>CreativeScroll Progress Bar</h2> 
         <section>
-          <h2>CreativeScroll Progress Bar</h2>
           <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima cupiditate accusamus ratione exercitationem
          blanditiis dolorum, aspernatur eos eligendi consequatur nihil. Ipsam architecto consectetur asperiores 
          natus labore, porro consequatur. Culpa, magni.
