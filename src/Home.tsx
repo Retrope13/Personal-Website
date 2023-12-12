cd2d6c0dd93ac01f038c5b3e37d119e933e4d151
Samuel Mckay <89430082+Retrope13@users.noreply.github.com>
2023-12-12 14:19:35 -0700

    add new styled elements for body seperation

--- src/Home.tsx
+++ src/Home.tsx
@@ -2,6 +2,7 @@ import './Home.css';
 import MyPhoto from './assets/imgs/SamMcKayPic.jpg';
 import headerImg from './assets/imgs/retrolineHeading.png'
 import footerImg from './assets/imgs/footerImg.png'
+import github from './assets/imgs/github.png'
 import React, {useState, useEffect} from 'react';
 import styled from 'styled-components';
 
@@ -10,26 +11,72 @@ const Head = styled.h1`
   position: absolute;
   z-index: 1;
   color: #ce7052; 
-  top: 15vh;
+  top: 3vh;
   left: 3vw;
   font-family: Alba;
-  font-size: 100px;
+  font-size: 8vw;
   background-color: #00000000;
   text-shadow: 
-    6px 6px 0 #dc9954, /*cream*/
-    10px 10px 0 #d3c598; /*Brown*/
+    6px 6px 0 #dc9954, /*light orange*/
+    10px 10px .2vw #6F5345; /*cream*/
+  `
+
+const StyledH2 = styled.h2`
+  position: relative;
+  display: inline;
+  margin-right: 2vw;
+  z-index: 1;
+  color: #ce7052; 
+  top: 3vh;
+  font-family: Alba;
+  font-size: 5vw;
+  background-color: #00000000;
+  text-shadow: 
+    6px 6px 0 #dc9954, /*light orange*/
+    10px 10px .2vw #6F5345; /*cream*/
+  `
+
+  const Content = styled.p`
+    position: relative;
+    font-family: Roca2;
+    font-size: 1.4vw;
+    left: 3vw;
+    color: #7C331D;
+    /*washed red: #D45456, aqua blue: #289C9D, */
+  `
+
+  const Icon = styled.img`
+    position: relative;
+    width: 7vw;
+    height: 11.75vh;
+    background-color: #6F5345;
+    border-radius: 1vw;
+    top: 4vh;
+
+    box-shadow:inset 2px 2px 2px 0px rgba(255,255,255,.5),
+    7px 7px 20px 0px rgba(0,0,0,.1),
+    4px 4px 5px 0px rgba(0,0,0,.1);
+
+    &:hover,
+    &:focus {
+      cursor: pointer;
+      box-shadow:inset 2px 2px 2px 0px rgba(255,255,255,.5),
+      7px 7px 20px 0px rgba(0,0,0,.1),
+      4px 4px 5px 0px rgba(0,0,0,.1);
+      transform: scale(1.1)
+    }
   `
 
 function Home() {
   const progress = document.getElementById("progressbar1")
   let totalHeight = document.body.scrollHeight - window.innerHeight;  
-  let startingTop = 10
+  let startingTop = 0
   if (progress) { 
     progress.style.top = startingTop + 'vh'
 
   }
   window.onscroll = function(){
-    let progressHeight = ((window.scrollY * 1.45) / totalHeight) * 100;
+    let progressHeight = ((window.scrollY * .5) / totalHeight) * 100;
     console.log(window.scrollY*1.5)
     console.log(window.innerHeight)
     if (progress) {
@@ -55,119 +102,21 @@ function Home() {
         <Head>
           Sam McKay
         </Head>
-          <h2>CreativeScroll Progress Bar</h2> 
         <section>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima cupiditate accusamus ratione exercitationem
-         blanditiis dolorum, aspernatur eos eligendi consequatur nihil. Ipsam architecto consectetur asperiores 
-         natus labore, porro consequatur. Culpa, magni.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero blanditiis reiciendis voluptas
-         a deleniti harum possimus iste autem modi temporibus recusandae eius eligendi placeat culpa, 
-         accusantium debitis enim voluptate at?
-         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima cupiditate accusamus ratione exercitationem
-         blanditiis dolorum, aspernatur eos eligendi consequatur nihil. Ipsam architecto consectetur asperiores 
-         natus labore, porro consequatur. Culpa, magni.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero blanditiis reiciendis voluptas
-         a deleniti harum possimus iste autem modi temporibus recusandae eius eligendi placeat culpa, 
-         accusantium debitis enim voluptate at?
-         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima cupiditate accusamus ratione exercitationem
-         blanditiis dolorum, aspernatur eos eligendi consequatur nihil. Ipsam architecto consectetur asperiores 
-         natus labore, porro consequatur. Culpa, magni.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero blanditiis reiciendis voluptas
-         a deleniti harum possimus iste autem modi temporibus recusandae eius eligendi placeat culpa, 
-         accusantium debitis enim voluptate at?
-         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima cupiditate accusamus ratione exercitationem
-         blanditiis dolorum, aspernatur eos eligendi consequatur nihil. Ipsam architecto consectetur asperiores 
-         natus labore, porro consequatur. Culpa, magni.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero blanditiis reiciendis voluptas
-         a deleniti harum possimus iste autem modi temporibus recusandae eius eligendi placeat culpa, 
-         accusantium debitis enim voluptate at?
-         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima cupiditate accusamus ratione exercitationem
-         blanditiis dolorum, aspernatur eos eligendi consequatur nihil. Ipsam architecto consectetur asperiores 
-         natus labore, porro consequatur. Culpa, magni.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero blanditiis reiciendis voluptas
-         a deleniti harum possimus iste autem modi temporibus recusandae eius eligendi placeat culpa, 
-         accusantium debitis enim voluptate at?
+          <StyledH2>About me</StyledH2>
+          <Content>
+            I'm a recent college graduate with a Bachelor of Science degree in Computer Science from Colorado State University. 
+            During my time at CSU I earned a 3.72 GPA, was on the Dean's list, and was an active member of the National Society for Collegiate Scholars. 
+            I am most passionate about web development and design but more recently I have been taking an interest in offensive cybersecurity and game development.
+            </Content>
+            <div id="h2Container">
+              <StyledH2>Projects</StyledH2><Icon src={github}/> 
+              {/* I need to add the <a> to make this actually take you to github^^ */}
 
-         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima cupiditate accusamus ratione exercitationem
-         blanditiis dolorum, aspernatur eos eligendi consequatur nihil. Ipsam architecto consectetur asperiores 
-         natus labore, porro consequatur. Culpa, magni.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero blanditiis reiciendis voluptas
-         a deleniti harum possimus iste autem modi temporibus recusandae eius eligendi placeat culpa, 
-         accusantium debitis enim voluptate at?
-         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima cupiditate accusamus ratione exercitationem
-         blanditiis dolorum, aspernatur eos eligendi consequatur nihil. Ipsam architecto consectetur asperiores 
-         natus labore, porro consequatur. Culpa, magni.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero blanditiis reiciendis voluptas
-         a deleniti harum possimus iste autem modi temporibus recusandae eius eligendi placeat culpa, 
-         accusantium debitis enim voluptate at?
-
-         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima cupiditate accusamus ratione exercitationem
-         blanditiis dolorum, aspernatur eos eligendi consequatur nihil. Ipsam architecto consectetur asperiores 
-         natus labore, porro consequatur. Culpa, magni.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero blanditiis reiciendis voluptas
-         a deleniti harum possimus iste autem modi temporibus recusandae eius eligendi placeat culpa, 
-         accusantium debitis enim voluptate at?
-         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima cupiditate accusamus ratione exercitationem
-         blanditiis dolorum, aspernatur eos eligendi consequatur nihil. Ipsam architecto consectetur asperiores 
-         natus labore, porro consequatur. Culpa, magni.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero blanditiis reiciendis voluptas
-         a deleniti harum possimus iste autem modi temporibus recusandae eius eligendi placeat culpa, 
-         accusantium debitis enim voluptate at?
-         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima cupiditate accusamus ratione exercitationem
-         blanditiis dolorum, aspernatur eos eligendi consequatur nihil. Ipsam architecto consectetur asperiores 
-         natus labore, porro consequatur. Culpa, magni.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero blanditiis reiciendis voluptas
-         a deleniti harum possimus iste autem modi temporibus recusandae eius eligendi placeat culpa, 
-         accusantium debitis enim voluptate at?
-         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima cupiditate accusamus ratione exercitationem
-         blanditiis dolorum, aspernatur eos eligendi consequatur nihil. Ipsam architecto consectetur asperiores 
-         natus labore, porro consequatur. Culpa, magni.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero blanditiis reiciendis voluptas
-         a deleniti harum possimus iste autem modi temporibus recusandae eius eligendi placeat culpa, 
-         accusantium debitis enim voluptate at?
-         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima cupiditate accusamus ratione exercitationem
-         blanditiis dolorum, aspernatur eos eligendi consequatur nihil. Ipsam architecto consectetur asperiores 
-         natus labore, porro consequatur. Culpa, magni.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero blanditiis reiciendis voluptas
-         a deleniti harum possimus iste autem modi temporibus recusandae eius eligendi placeat culpa, 
-         accusantium debitis enim voluptate at?
-         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima cupiditate accusamus ratione exercitationem
-         blanditiis dolorum, aspernatur eos eligendi consequatur nihil. Ipsam architecto consectetur asperiores 
-         natus labore, porro consequatur. Culpa, magni.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero blanditiis reiciendis voluptas
-         a deleniti harum possimus iste autem modi temporibus recusandae eius eligendi placeat culpa, 
-         accusantium debitis enim voluptate at?
-
-         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima cupiditate accusamus ratione exercitationem
-         blanditiis dolorum, aspernatur eos eligendi consequatur nihil. Ipsam architecto consectetur asperiores 
-         natus labore, porro consequatur. Culpa, magni.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero blanditiis reiciendis voluptas
-         a deleniti harum possimus iste autem modi temporibus recusandae eius eligendi placeat culpa, 
-         accusantium debitis enim voluptate at?Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima cupiditate accusamus ratione exercitationem
-         blanditiis dolorum, aspernatur eos eligendi consequatur nihil. Ipsam architecto consectetur asperiores 
-         natus labore, porro consequatur. Culpa, magni.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero blanditiis reiciendis voluptas
-         a deleniti harum possimus iste autem modi temporibus recusandae eius eligendi placeat culpa, 
-         accusantium debitis enim voluptate at?
-         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima cupiditate accusamus ratione exercitationem
-         blanditiis dolorum, aspernatur eos eligendi consequatur nihil. Ipsam architecto consectetur asperiores 
-         natus labore, porro consequatur. Culpa, magni.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero blanditiis reiciendis voluptas
-         a deleniti harum possimus iste autem modi temporibus recusandae eius eligendi placeat culpa, 
-         accusantium debitis enim voluptate at?
-         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima cupiditate accusamus ratione exercitationem
-         blanditiis dolorum, aspernatur eos eligendi consequatur nihil. Ipsam architecto consectetur asperiores 
-         natus labore, porro consequatur. Culpa, magni.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero blanditiis reiciendis voluptas
-         a deleniti harum possimus iste autem modi temporibus recusandae eius eligendi placeat culpa, 
-         accusantium debitis enim voluptate at?
-         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima cupiditate accusamus ratione exercitationem
-         blanditiis dolorum, aspernatur eos eligendi consequatur nihil. Ipsam architecto consectetur asperiores 
-         natus labore, porro consequatur. Culpa, magni.
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero blanditiis reiciendis voluptas
-         a deleniti harum possimus iste autem modi temporibus recusandae eius eligendi placeat culpa, 
-         accusantium debitis enim voluptate at?
-          </p>
+            </div>
+            <Content>
+            I have worked on several projects during my time at CSU but I have also created many personal projects. All of the code for the projects can be found on Github.
+          </Content>
         <img id="footerImg" src={footerImg}/>
         </section>
       </body>
