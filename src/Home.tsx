7a547272b6bfd36997241d972ae0646642c824f0
Samuel Mckay <89430082+Retrope13@users.noreply.github.com>
2023-12-20 14:20:18 -0700

    Stashing a bunch of sizing changes

--- src/Home.tsx
+++ src/Home.tsx
@@ -15,40 +15,52 @@ import { useEffect, useState } from 'react';
 const HeadShot = styled.img`
   position: absolute;
   z-index: 2;
-  border-radius: 10%;
-  border: .5vw solid #DC9954;
-  left: 4vw;
-  top: 7vh;
-  width: 600px;
+  border-radius: 20px;
+  border: 7px solid #DC9954;
+  left: 80px;
+  top: 5vh;
+  width: 30vw;
 `
 
 const Head = styled.h1`
   position: absolute;
-  z-index: 1;
+  z-index: 2;
   color: #ce7052; 
-  top: 10vh;
-  left: 39vw;
+  top: 120px;
+  left: 750px;
   font-family: Alba;
   font-size: 150px;
   background-color: #00000000;
+  white-space: nowrap;
   text-shadow: 
-    .25vw .25vw 0 #dc9954, /*light orange*/
-    .5vw .5vw .2vw #6F5345; /*cream*/
+    7px 7px 0 #dc9954, /*light orange*/
+    14px 14px 3px #6F5345; /*cream*/
   `
+
+const Subtitle = styled.h3`
+  position: absolute;
+  z-index: 2;
+  color: #6F5345; 
+  top: 37vh;
+  left: 40vw;
+  font-family: Roca2;
+  font-size: 30px;
+  white-space: nowrap;
+` //~Change the font family for this 
 
 const StyledH2 = styled.h2`
   position: relative;
   display: inline;
-  margin-right: 2vw;
+  margin-right: 50px;
   z-index: 1;
   color: #ce7052; 
-  top: 3vh; 
+  top: 30px; 
   font-family: Alba;
   font-size: 100px;
   background-color: #00000000;
   text-shadow: 
-    .25vw .25vw 0 #dc9954, /*light orange*/
-    .5vw .5vw .2vw #6F5345; /*cream*/
+    5px 5px 0 #dc9954, /*light orange*/
+    10px 10px 3px #6F5345; /*cream*/
     `
 
   //*Jenelle's website has the buttons faded, then when you hover on them they saturate and grow.
@@ -57,17 +69,17 @@ const StyledH2 = styled.h2`
     width: 100px;
     height: 100px;
     background-color: #6F5345;
-    border-radius: 1vw;
-    top: 4vh;
-    z-index: 3;
-    overflow: none;
+    border-radius: 20px;
+    top: 50px;
+    z-index: 3;  
+    overflow: none; 
     cursor: pointer;
 
 
     //! I might want to change this to an angled drop shadown but idk
-    box-shadow:inset 0vw 0vw 0vw 0vw rgba(255,255,255,.5),
-    0vw 0vw .2vw .2vh rgba(0,0,0,1),
-    1vw 1vw 1vw 0vw rgba(0,0,0,.1);
+    box-shadow:inset 0px 0px 0px 0px rgba(255,255,255,.5),
+    0px 0px 5px 2px rgba(0,0,0,1),
+    10px 10px 10px 0px rgba(0,0,0,.1);
 
     &:active,
     &:focus {
@@ -82,7 +94,8 @@ const StyledH2 = styled.h2`
     position: relative;
     font-family: Roca2;
     font-size: 30px;
-    left: 3vw;
+    left: 70px;
+    width: 70vw;
     color: #7C331D;
     /*washed red: #D45456, aqua blue: #289C9D, */
   `
@@ -91,20 +104,17 @@ const StyledH2 = styled.h2`
     display: flex;
     justify-content: center;
     width: 100vw;
-    top: -3vh;
-    left: -4vw;
+    top: -30px;
+    left: -60px;
     z-index: 1;
     color: #ce7052; 
     font-family: Alba;
-    font-size: 5vw;
+    font-size: 100px;
     background-color: #00000000;
     text-shadow: 
-      .25vw .25vw 0 #dc9954, /*light orange*/
-      .5vw .5vw .2vw #6F5345; /*cream*/
+      5px 5px 0 #dc9954, /*light orange*/
+      10px 10px 3px #6F5345; /*cream*/
   `
-
-
-//~When I look at the mobile version right now the page is really zoomed out. I think it's because of overflow rules?
 
 function Home() {
 
@@ -115,9 +125,9 @@ function Home() {
   //^ the styling for the icons that are placed at the bottom of the page
   const BottomIcons = {  
     display: "inline-block",
-    width: '6vw',
-    height: '6vw',
-    top: "0vh"
+    width: '115px',
+    height: '115px',
+    top: "-20px"
   }
 
   //^ the styling for the sentence about contacts at the bottom.
@@ -126,14 +136,14 @@ function Home() {
     justifyContent: "center",
     width: "100vw",
     position: "relative",
-    left: "-3vw",
-    top: "-10vh"
+    left: "-60px",
+    top: "-120px",
   } as React.CSSProperties;
 
   //^The icon for my resume
   const resumeIcon = {
-    width: '6.3vw',
-    height: '6.7vw'
+    width: '115px',
+    height: '115px'
   }
 
 
@@ -143,13 +153,13 @@ function Home() {
       let progressHeight;
       if (totalHeight) {
         progressHeight = ((window.scrollY * .7) / totalHeight) * 100;  
-        console.log(window.scrollY*1.5)
-        console.log(window.innerHeight)
+        console.log(window.scrollY*1.5);
+        console.log(window.innerHeight);
 
       }
       if (progress) {
-        progress.style.height = progressHeight  + "vh"  
-        progress.style.top = (0 + 'vh') 
+        progress.style.height = progressHeight  + "vh";
+        progress.style.top = (0 + 'px'); 
       }
 
 }
@@ -160,7 +170,16 @@ function Home() {
     <div className="App">
       <div className="MyComponent"/>
       <title>Home</title>
-      <HeadShot alt="An image of myself wearing a blue sport jacket and a pinstripe blue and white collared shirt. I am standing in front of a headge of bushes." src={Headshot}></HeadShot>
+      <div id="heading_container">
+        <HeadShot alt="An image of myself wearing a blue sport jacket and a pinstripe blue and white collared shirt. I am standing in front of a headge of bushes." src={Headshot}></HeadShot>
+        <Head>
+            Sam McKay
+            <br/>
+          </Head>
+            <Subtitle>
+            WEB DEVELOPER AND SOFTWARE ENGINEER
+              </Subtitle>
+      </div>
       <img id='headerImg' alt='a stylistic pattern of two rounded lines intersecting eachother on the right side of the screen, The lines are brown, burnt orange, light orange, and a gray cream.' src={headerImg}/>
       <div className="Heading">
         <div className ='hider'>
@@ -171,9 +190,6 @@ function Home() {
 
       </div> 
       <body> 
-        <Head>
-          Sam McKay
-        </Head>
         <section>
           <StyledH2>About Me</StyledH2>
           <Content>
@@ -211,15 +227,15 @@ function Home() {
             <iframe title='resume viewer' src={Resume}></iframe>
           </Content> 
 
-        <img id="footerImg" alt='A stylistic rounded line that curves downward and to the right off of the screen. The lines are brown, burnt orange, light orange, and a gray cream. ' src={footerImg}/> 
           <ContactH2>Contact Me</ContactH2>
-            <Content style={ContactContent}>Feel free to contact me through Github, Email, or LinkedIn!</Content> 
+            <Content style={ContactContent}>Feel free to contact me through Github, Email, or LinkedIn!</Content>   
           <div id='contactDiv'>
           <a href="https://github.com/Retrope13" target='_blank' rel="noreferrer noopener"><Icon alt="An icon of the github logo. It is in a white, minimalistic style with a brown background." src={github} style={BottomIcons}/> </a>
           <a href="mailto:mckaypable@gmail.com" target='_blank' rel="noreferrer noopener"><Icon alt="An icon of an envelope. It is in a white, minimalistic style with a brown background." src={email} style={BottomIcons}/> </a>
           <a href="https://www.linkedin.com/in/sam-mckay13/" target='_blank' rel="noreferrer noopener"><Icon alt="An icon of the LinkedIn logo. It is in a white minimalistic style with a brown background." src={LinkedIn} style={BottomIcons}/> </a>
           </div>
         </section>
+        <img id="footerImg" alt='A stylistic rounded line that curves downward and to the right off of the screen. The lines are brown, burnt orange, light orange, and a gray cream. ' src={footerImg}/> 
       </body>
     </div>
   );
