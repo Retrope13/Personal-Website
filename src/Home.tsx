ef1c2128c0c2a4505626d1de853343cc08978d5a
Samuel Mckay <89430082+Retrope13@users.noreply.github.com>
2023-12-20 23:10:58 -0700

    converted everything to CSS control and made second ruleset for mobile

--- src/Home.tsx
+++ src/Home.tsx
@@ -12,109 +12,11 @@ import Download from './assets/imgs/Download.png';
 import styled from 'styled-components';
 import { useEffect, useState } from 'react';
 
-const HeadShot = styled.img`
-  position: absolute;
-  z-index: 2;
-  border-radius: 20px;
-  border: 7px solid #DC9954;
-  left: 80px;
-  top: 45px;
-  width: 600px;
-`
-
-const Head = styled.h1`
-  position: absolute;
-  z-index: 2;
-  color: #ce7052; 
-  top: 120px;
-  left: 750px;
-  font-family: Alba;
-  font-size: 150px;
-  background-color: #00000000;
-  white-space: nowrap;
-  text-shadow: 
-    7px 7px 0 #dc9954, /*light orange*/
-    14px 14px 3px #6F5345; /*cream*/
-  `
-
-const Subtitle = styled.h3`
-  position: absolute;
-  z-index: 2;
-  color: #6F5345; 
-  top: 400px;
-  left: 750px;
-  font-family: Roca2;
-  font-size: 30px;
-  white-space: nowrap;
-` //~Change the font family for this 
-
-const StyledH2 = styled.h2`
-  position: relative;
-  display: inline;
-  margin-right: 50px;
-  z-index: 1;
-  color: #ce7052; 
-  top: 30px; 
-  font-family: Alba;
-  font-size: 100px;
-  background-color: #00000000;
-  text-shadow: 
-    5px 5px 0 #dc9954, /*light orange*/
-    10px 10px 3px #6F5345; /*cream*/
-    `
+//!!I originally had a lot of styled react elements and then decided to place 
+//!!them in the css file so I had more control over them when changing screen sizes
 
   //*Jenelle's website has the buttons faded, then when you hover on them they saturate and grow.
-  const Icon = styled.img`
-    position: relative;
-    width: 100px;
-    height: 100px;
-    background-color: #6F5345;
-    border-radius: 20px;
-    top: 50px;
-    z-index: 3;  
-    overflow: none; 
-    cursor: pointer;
-
-
-    //! I might want to change this to an angled drop shadown but idk
-    box-shadow:inset 0px 0px 0px 0px rgba(255,255,255,.5),
-    0px 0px 5px 2px rgba(0,0,0,1),
-    10px 10px 10px 0px rgba(0,0,0,.1);
-
-    &:active,
-    &:focus {
-      box-shadow:inset 2px 2px 2px 0px rgba(255,255,255,.5),
-      7px 7px 20px 0px rgba(0,0,0,.1),
-      4px 4px 5px 0px rgba(0,0,0,.1);
-      transform: scale(.95); //*I might want to change this
-    }
-  `
       //^ maybe I should add a background div to the content so it stands out more?
-  const Content = styled.p`
-    position: relative;
-    font-family: Roca2;
-    font-size: 30px;
-    left: 70px;
-    width: 1350px; 
-    color: #7C331D;
-    /*washed red: #D45456, aqua blue: #289C9D, */ 
-  `
-  const ContactH2 = styled.h2`
-    position: relative;
-    display: flex;
-    justify-content: center;
-    width: 1920px;
-    top: -30px;
-    left: -60px;
-    z-index: 1;
-    color: #ce7052; 
-    font-family: Alba;
-    font-size: 100px;
-    background-color: #00000000;
-    text-shadow: 
-      5px 5px 0 #dc9954, /*light orange*/
-      10px 10px 3px #6F5345; /*cream*/
-  `
 
 function Home() {
 
@@ -122,29 +24,6 @@ function Home() {
   const totalHeight = document.body.scrollHeight - window.innerHeight; 
 
 
-  //^ the styling for the icons that are placed at the bottom of the page
-  const BottomIcons = {  
-    display: "inline-block",
-    width: '115px',
-    height: '115px',
-    top: "-20px"
-  }
-
-  //^ the styling for the sentence about contacts at the bottom.
-  const ContactContent = {
-    display: "flex",
-    justifyContent: "center",
-    width: "1920px",
-    position: "relative",
-    left: "-60px",
-    top: "-120px",
-  } as React.CSSProperties;
-
-  //^The icon for my resume
-  const resumeIcon = {
-    width: '115px',
-    height: '115px'
-  }
 
 
 
@@ -170,18 +49,18 @@ function Home() {
     <div className="App">
       <div className="MyComponent"/>
       <title>Home</title>
-      <div id="heading_container">
-        <HeadShot alt="An image of myself wearing a blue sport jacket and a pinstripe blue and white collared shirt. I am standing in front of a headge of bushes." src={Headshot}></HeadShot>
-        <Head>
+      <img className='headerImg' alt='a stylistic pattern of two rounded lines intersecting eachother on the right side of the screen, The lines are brown, burnt orange, light orange, and a gray cream.' src={headerImg}/>
+      <div className="heading_container">
+        <img className='headShot' alt="An image of myself wearing a blue sport jacket and a pinstripe blue and white collared shirt. I am standing in front of a headge of bushes." src={Headshot}></img>
+        <h1 className='head'>
             Sam McKay
             <br/>
-          </Head> 
-            <Subtitle>
+          </h1> 
+            <h3 className='subtitle'>
             WEB DEVELOPER AND SOFTWARE ENGINEER
-              </Subtitle>
+              </h3>
       </div>
-      <img id='headerImg' alt='a stylistic pattern of two rounded lines intersecting eachother on the right side of the screen, The lines are brown, burnt orange, light orange, and a gray cream.' src={headerImg}/>
-      <div className="Heading">
+      <div className="heading">
         <div className ='hider'>
         </div>
           <div id="scrollpath">  
@@ -192,14 +71,14 @@ function Home() {
       </div> 
       <body> 
         <section>
-          <StyledH2>About Me</StyledH2>
-          <Content>
+          <h2 className='styledH2'>About Me</h2>
+          <p className='content'>
             I'm a recent college graduate with a Bachelor of Science degree in Computer Science from Colorado State University. 
             During my time at CSU I earned a 3.72 GPA, was on the Dean's list, and was an active member of the National Society for Collegiate Scholars. 
             I am most passionate about web development and design but more recently I have been taking an interest in offensive cybersecurity and game development.
-            </Content>
-              <StyledH2>Projects</StyledH2><a href="https://github.com/Retrope13" target='_blank' rel="noreferrer noopener"><Icon src={github} alt="The Github logo done in a white, minimalistic style with a brown background."/></a> {/* I might also want to use framer motion to make the button hover effect look cooler */}
-            <Content>
+            </p>
+              <h2 className='styledH2'>Projects</h2><a href="https://github.com/Retrope13" target='_blank' rel="noreferrer noopener"><img className='icons' src={github} alt="The Github logo done in a white, minimalistic style with a brown background."/></a> {/* I might also want to use framer motion to make the button hover effect look cooler */}
+            <p className='content'>
             I have worked on several projects during my time at CSU but I have also created many personal projects. All of the code for my projects can be found on Github. The projects I am most proud of are: "Gone Fishing", and "Bidder, Faster, Stronger".
             <br/><br/>
             In the 'Gone Fishing' project, I designed a frontend featuring three cards, each displaying a cartoon frog image created using Inkscape. 
@@ -221,19 +100,19 @@ function Home() {
              I would assess the highest bid, transfer the highest bid to my wallet, and the participant with the highest bid would be awarded the corresponding NFT.
              <br/>
              This project provided an invaluable learning experience about blockchain technology, and I utilized Ganache as the testing system. I take great pride in the successful completion of this project.
-          </Content>
+          </p>
 
-          <StyledH2>Resume</StyledH2> <a href={Resume} rel="noreferrer noopener" target="_blank">  <Icon alt="A download icon with an arrow pointing downward into a shallow receptical. It is in a white, minimalistic style with a brown background." style={resumeIcon} src={Download}/> </a>
-          <Content> 
+          <h2 className='styledH2'>Resume</h2><a href={Resume} rel="noreferrer noopener" target="_blank"><img className='icons' alt="A download icon with an arrow pointing downward into a shallow receptical. It is in a white, minimalistic style with a brown background." src={Download}/></a>
+          <p className='content'> 
             <iframe title='resume viewer' src={Resume}></iframe>
-          </Content> 
+          </p> 
 
-          <ContactH2>Contact Me</ContactH2>
-            <Content style={ContactContent}>Feel free to contact me through Github, Email, or LinkedIn!</Content>   
+          <h2 id='contactH2'>Contact Me</h2>
+          <p  className="content" id="contactContent">Contact me through Github, Email, or LinkedIn!</p>   
           <div id='contactDiv'>
-          <a href="https://github.com/Retrope13" target='_blank' rel="noreferrer noopener"><Icon alt="An icon of the github logo. It is in a white, minimalistic style with a brown background." src={github} style={BottomIcons}/> </a>
-          <a href="mailto:mckaypable@gmail.com" target='_blank' rel="noreferrer noopener"><Icon alt="An icon of an envelope. It is in a white, minimalistic style with a brown background." src={email} style={BottomIcons}/> </a>
-          <a href="https://www.linkedin.com/in/sam-mckay13/" target='_blank' rel="noreferrer noopener"><Icon alt="An icon of the LinkedIn logo. It is in a white minimalistic style with a brown background." src={LinkedIn} style={BottomIcons}/> </a>
+          <a href="https://github.com/Retrope13" target='_blank' rel="noreferrer noopener"><img className='icons' id="bottomIcons" alt="An icon of the github logo. It is in a white, minimalistic style with a brown background." src={github}/></a>
+          <a href="mailto:mckaypable@gmail.com" target='_blank' rel="noreferrer noopener"><img className='icons' id="bottomIcons" alt="An icon of an envelope. It is in a white, minimalistic style with a brown background." src={email}/></a>
+          <a href="https://www.linkedin.com/in/sam-mckay13/" target='_blank' rel="noreferrer noopener"><img className='icons' id="bottomIcons" alt="An icon of the LinkedIn logo. It is in a white minimalistic style with a brown background." src={LinkedIn}/></a>
           </div>
         <img id="footerImg" alt='A stylistic rounded line that curves downward and to the right off of the screen. The lines are brown, burnt orange, light orange, and a gray cream. ' src={footerImg}/> 
         </section>
